ee49b0db6039f1a1bcfdaeb7dded7e5bffba3ff9
henrik.steengaard <henrik.steengaard@valtech.com>
2017-06-14 09:33:28 +0200

    Updated patch assembly files script

--- ci/powershell/patch_assembly_version.ps1
+++ ci/powershell/patch_assembly_version.ps1
@@ -20,7 +20,7 @@ try
     }
 
     # patch prerelease to last 20 characters, if prerelease exceeds 20 characters
-    if ($prerelease.length -gt 21)
+    if ($prerelease.length -gt 20)
     {
         $prerelease = "-" + $prerelease.Substring($prerelease.length - 20, 20)
     }
